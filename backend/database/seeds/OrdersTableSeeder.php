fe673ca1ab1463ed0792507e0ef96569da9d8c9f
vunguyenhung <vunguyenhung@outlook.com>
2017-04-28 03:40:41 +0700

    * improve datetime

--- backend/database/seeds/OrdersTableSeeder.php
+++ backend/database/seeds/OrdersTableSeeder.php
@@ -8,15 +8,20 @@ namespace database\seeds;
 
 use App\Entities\Order;
 use App\Entities\ShoppingCart;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class OrdersTableSeeder extends Seeder {
 
     public function run() {
+        $faker = Factory::create();
         $inactiveShoppingCarts = ShoppingCart::where('active', false)->get();
         foreach ($inactiveShoppingCarts as $inactiveShoppingCart) {
+            $creationTime = $faker->dateTimeBetween($inactiveShoppingCart->updated_at, 'now');
             factory(Order::class)->create([
-                'shopping_cart_id' => $inactiveShoppingCart->id
+                'shopping_cart_id' => $inactiveShoppingCart->id,
+                'created_at' => $creationTime,
+                'updated_at' => $creationTime
             ]);
         }
     }
