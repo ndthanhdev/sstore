aeb071e6acbed0f8020166e3fb4d2768f776e7c5
vunguyenhung <vunguyenhung@outlook.com>
2017-04-28 11:25:29 +0700

    * improve fake name

--- backend/database/factories/OrderFactory.php
+++ backend/database/factories/OrderFactory.php
@@ -6,7 +6,7 @@
 $factory->define(\App\Entities\Order::class, function (Faker\Generator $faker) {
     $dateTimeThisYear = $faker->dateTimeThisYear;
     return [
-        'code' => $faker->unique()->regexify('SS[0-9]{5}'),
+        'code' => $faker->unique()->regexify('SSORD[0-9]{5}'),
         'rating' => $faker->optional(0.6)->numberBetween(1, 5),
         'comment' => $faker->optional(0.6)->sentence(),
         'state' => 3, // default is DONE!
