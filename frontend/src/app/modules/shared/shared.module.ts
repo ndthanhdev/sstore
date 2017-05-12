da813fa3bc9e426c753aefd4ae43c0a4fc4c39ef
vunguyenhung <vunguyenhung@outlook.com>
2017-05-12 19:56:01 +0700

    * completely remove product-reviews component

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -1,17 +1,18 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProductVariationValuesComponent} from './product-variation-values/product-variation-values.component';
-import {ProductReviewsComponent} from './product-reviews/product-reviews.component';
 import {ProductReviewRatingComponent} from './product-review-rating/product-review-rating.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ProductVariationValuesComponent, ProductReviewsComponent, ProductReviewRatingComponent],
+  declarations: [
+    ProductVariationValuesComponent,
+    ProductReviewRatingComponent
+  ],
   exports: [
     ProductVariationValuesComponent,
-    ProductReviewsComponent,
     ProductReviewRatingComponent
   ]
 })
