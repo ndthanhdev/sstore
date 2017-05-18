e745b042789407a8dde758d9807261d823b8f67e
vunguyenhung <vunguyenhung@outlook.com>
2017-05-19 00:36:15 +0700

    * improve store factory's latitude and longitude

--- backend/database/factories/StoreFactory.php
+++ backend/database/factories/StoreFactory.php
@@ -8,8 +8,8 @@ $factory->define(\App\Entities\Store::class, function (Faker\Generator $faker) {
         'name' => $faker->city,
         'address' => $faker->address,
         //TODO: collect real latitude and longitude
-        'latitude' => $faker->latitude($min = 10, $max = 11),
-        'longitude' => $faker->longitude($min = 106, $max = 107),
+        'latitude' => $faker->latitude($min = 10.7, $max = 10.9),
+        'longitude' => $faker->longitude($min = 106.6, $max = 106.9),
         'primary' => false
     ];
 });
