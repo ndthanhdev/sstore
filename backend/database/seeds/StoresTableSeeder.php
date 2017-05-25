74b8c31e7cb55e79c5b01d9c5aa0b8eb33864a2a
vunguyenhung <vunguyenhung@outlook.com>
2017-05-25 22:17:28 +0700

    - fix role bug

--- backend/database/seeds/StoresTableSeeder.php
+++ backend/database/seeds/StoresTableSeeder.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Seeder;
 
 class StoresTableSeeder extends Seeder {
     public function run() {
-        $managerAccounts = Account::where('role', 2)->get();
+        $managerAccounts = Account::where('role', 1)->get();
         factory(Store::class)->states(['primary'])->create([
             'manager_id' => $managerAccounts->first()->user->id
         ]);
