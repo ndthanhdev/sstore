7ee039c6751078984f25ac5d5b609331814c184a
ThanhTDT <thanhtdt@outlook.com>
2017-05-26 14:01:45 -0700

    add iot project

---
+++ Things/Master/Master.ino
@@ -0,0 +1,96 @@
+/*
+ Name:		Master.ino
+ Created:	3/27/2017 8:46:06 PM
+ Author:	duyth
+*/
+
+#include <SoftwareSerial.h>
+
+#define BAUD (9600)
+
+#define RX 8
+#define TX 9
+
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+byte mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+IPAddress server(10, 35, 19, 22);
+
+SoftwareSerial commandTransporter(RX, TX);
+
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
+void setup() {
+
+	Serial.begin(BAUD);
+	Serial.println("initializing...");
+
+	setupEthernetShield();
+
+	commandTransporter.begin(BAUD);	
+
+	client.setServer("test.mosquitto.org", 1883);
+
+	client.setCallback(callback);
+
+	
+	
+	// Allow the hardware to sort itself out
+	delay(1500);
+	Serial.println("initialized.");
+}
+
+void loop() {
+	if (!client.connected()) {
+		reconnect();
+	}
+	client.loop();
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+	Serial.print("Message arrived [");
+	Serial.print(topic);
+	Serial.print("] ");
+	String inString((char*)payload);
+	Serial.println(inString);
+	commandTransporter.println(inString.toInt());
+}
+
+void reconnect() {
+	// Loop until we're reconnected
+	while (!client.connected()) {
+		Serial.print("Attempting MQTT connection...");
+		// Attempt to connect
+		if (client.connect("arduinoClient")) {
+			Serial.println("connected");
+			// Once connected, publish an announcement...
+			client.publish("a", "hello server");
+			// ... and resubscribe
+			client.subscribe("s");
+		}
+		else {
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			Serial.println(" try again in 5 seconds");
+			// Wait 5 seconds before retrying
+			delay(5000);
+		}
+	}
+}
+
+void setupEthernetShield() {
+	// start the Ethernet connection:
+	if (Ethernet.begin(mac) == 0) {
+		Serial.println("Failed to configure Ethernet using DHCP");
+		// no point in carrying on, so do nothing forevermore:
+		for (;;)
+			;
+	}
+	// print your local IP address:
+	Serial.println(Ethernet.localIP());
+}
---
+++ Things/Servant/Servant.ino
@@ -0,0 +1,109 @@
+﻿/*
+ Name:		Servant.ino
+ Created:	3/27/2017 8:43:08 PM
+ Author:	duyth
+*/
+
+#include <SoftwareSerial.h>
+
+#define BAUD (9600)	
+
+//	DRV8825
+#define ENABLE 4	
+#define STEP 3	
+#define DIR 2	
+
+#define PHOTO_RESISTOR 7	//	chân DO của quang trở
+
+#define RX 8	//
+#define TX 9	//
+
+
+
+SoftwareSerial commandTransporter(RX, TX);
+
+Stream *inputStream;
+
+void setup()
+{
+	setupMotor();
+	setupPhotoresistor();
+
+	Serial.begin(BAUD);
+
+	// if release
+	commandTransporter.begin(BAUD);
+	inputStream = &commandTransporter;
+
+	// if debug	
+	//inputStream = &Serial;
+	
+	
+}
+
+void loop()
+{
+	if (inputStream->available())
+	{
+		Serial.println("Recieved command");
+		releasePayload(inputStream->parseInt());
+	}
+}
+
+void setupMotor() {
+	pinMode(STEP, OUTPUT);
+	pinMode(DIR, OUTPUT);
+	pinMode(ENABLE, OUTPUT);
+
+	disableMotor();
+}
+
+void doQuarterRevolution() {
+	int x;
+	for (x = 0; x < 50; x++)
+	{
+		digitalWrite(STEP, HIGH);
+		delayMicroseconds(1000);
+		digitalWrite(STEP, LOW);
+		delayMicroseconds(1000);
+	}
+}
+
+void setupPhotoresistor() {
+	pinMode(PHOTO_RESISTOR, INPUT);
+}
+
+bool isPayloadCrossed(int waitTime = 250) {
+	int debounce = 0;
+	long current = millis();
+	while (millis() - current < waitTime) {
+		if (digitalRead(PHOTO_RESISTOR) == HIGH)
+			debounce += 1;
+		delay(1);
+	}
+	return debounce > 3;
+}
+
+void releasePayload(int n) {
+	enableMotor();
+	while (n > 0)
+	{
+		while (!isPayloadCrossed()) {
+			doQuarterRevolution();
+		}
+		n--;
+		Serial.println("Released 1 package");
+		delay(1000); // wait for package pass
+	}
+	disableMotor();
+}
+
+void enableMotor() {
+	digitalWrite(ENABLE, LOW);
+}
+
+void disableMotor() {
+	digitalWrite(ENABLE, HIGH);
+}
+
+
