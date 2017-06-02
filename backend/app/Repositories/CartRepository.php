f6495670b308f372a903b553c8e1e69eab4d257c
vunguyenhung <vunguyenhung@outlook.com>
2017-06-02 22:04:37 +0700

    - refactor get active cart feature

--- backend/app/Repositories/CartRepository.php
+++ backend/app/Repositories/CartRepository.php
@@ -7,6 +7,7 @@ namespace App\Repositories;
 
 
 use App\Entities\ShoppingCart;
+use Illuminate\Support\Facades\DB;
 use Prettus\Repository\Contracts\CacheableInterface;
 use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Traits\CacheableRepository;
@@ -38,8 +39,10 @@ class CartRepository extends BaseRepository implements CacheableInterface {
     }
 
     public function getActiveCart($userId) {
-        return $this->withCount('details AS product')
-            ->findWhere(['user_id' => $userId, 'active' => true])
-            ->first();
+        return ShoppingCart::where(['user_id' => $userId, 'active' => true])
+            ->join('shopping_cart_details', 'shopping_carts.id', '=', 'shopping_cart_details.shopping_cart_id')
+            ->select('shopping_carts.id', DB::raw('SUM(shopping_cart_details.quantity) as item_count'))
+            ->groupBy('shopping_carts.id')
+            ->get();
     }
 }
